fb0f8125df52969f5bfc743bb421d41d59ec749f
aakash <aakashpaswan418giet@gmail.com>
2026-08-01 16:34:04 +0530

    Add Numbers example

---
+++ variables/Numbers.java
@@ -0,0 +1,30 @@
+public class Numbers{
+	public static void main(String[]args){
+		// Numbers values
+		//1. Integer types->byte, short, int
+		//2. Floating types->float and double.
+		
+		byte b = 100;
+		System.out.println(b);
+		
+		short s = 1000;
+		System.out.println(s);
+		
+		int i = 50000;
+		System.out.println(i);
+		
+		long myNum = 15000000000L; // long store large values when int is not enough to store large value.
+		System.out.println(myNum);
+		
+		
+		// Floating point Types 
+		
+		float f = 1.52f;
+		System.out.println(f);
+		
+		double d=33.33d;
+		System.out.println(d); // large floating point numbers.
+
+
+	}
+}
